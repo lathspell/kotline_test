5faa831d68579ff95fd4885d61d651aa249cb455
Christian Brunotte <cb@lathspell.de>
2018-07-12 01:23:43 +0200

    ...

--- src/test/kotlin/ByteLiteralsTest.kt
+++ src/test/kotlin/ByteLiteralsTest.kt
@@ -1,7 +1,7 @@
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class ByteLiteralTest {
+class ByteLiteralsTest {
 
     @Test
     fun testByteLiteral() {
---
+++ src/test/kotlin/LateInitTest.kt
@@ -0,0 +1,38 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class LateInitTest {
+    // just to keep track of the execution order
+    companion object {
+        private val order = mutableListOf<String>()
+    }
+
+    // imagine a data class for application preferences
+    data class Prefs(val color : String) {
+        init {
+            order.add("prefs-init")
+        }
+    }
+
+    // imagine a loader that reads an .ini file
+    class PrefsLoader {
+        fun loadPrefs(): Prefs {
+            order.add("loadPrefs")
+            return Prefs("red")
+        }
+    }
+
+    /** By lazy-loading the "prefs" variable, the preference loading is deferred until the variable is actually be accessed. */
+    @Test
+    fun test() {
+        order.add("before")
+        val prefs: Prefs by lazy {
+            PrefsLoader().loadPrefs()
+        }
+        order.add("after")
+
+        assertNotNull(prefs)
+        assertEquals(listOf("before", "after", "loadPrefs", "prefs-init"), order)
+    }
+}
