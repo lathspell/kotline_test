3c21fbe813e128f61b7d8f4b6bc6c41c54021265
Christian Brunotte <cb@lathspell.de>
2018-08-03 01:07:01 +0200

    git

--- lang/src/test/kotlin/de/lathspell/test/MapReduceTest.kt
+++ lang/src/test/kotlin/de/lathspell/test/MapReduceTest.kt
@@ -7,6 +7,18 @@ class MapReduceTest {
 
     private val colors = listOf("red", "green", "blue", "black")
 
+    @Test
+    fun map() {
+        assertEquals(listOf(3, 5, 4, 5), colors.map { it.length })
+    }
+
+    @Test
+    fun reduce() {
+        val actual = colors.reduce { result, value -> "$result, $value" }
+        assertEquals("red, green, blue, black", actual)
+    }
+
+
     /**
      * The flatMap() function can be used to iterate over a list and add/remove elements to it on the fly.
      */
@@ -22,9 +34,18 @@ class MapReduceTest {
         assertEquals(listOf("red", "blue", "blue", "black", "black"), actual)
     }
 
+    /**
+     * Fold combines all values to a single one.
+     * "acc" stands for "accumulator"
+     * "it" stands for "iterator"
+     */
     @Test
-    fun reduce() {
-        val actual = colors.reduce { result, value -> "$result, $value" }
-        assertEquals("red, green, blue, black", actual)
+    fun fold() {
+        val numbers = listOf(4, 8, 13)
+        assertEquals(29, 4 + numbers.sum())
+        assertEquals(29, numbers.fold(4) { acc, it -> acc + it })
+
+        // find the greatest number
+        assertEquals(13, numbers.fold(numbers.first()) {max, it -> if(it>max) it else max})
     }
 }
