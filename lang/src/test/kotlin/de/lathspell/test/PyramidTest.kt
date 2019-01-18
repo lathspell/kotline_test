e3ab5c092c143f0f3329ad4062ebfd194efba960
Christian Brunotte <cb@lathspell.de>
2019-01-18 20:48:24 +0100

    Kotlin is beautiful.

---
+++ lang/src/test/kotlin/de/lathspell/test/PyramidTest.kt
@@ -0,0 +1,19 @@
+package de.lathspell.test
+
+import org.assertj.core.api.Assertions.assertThat
+import kotlin.test.Test
+
+class PyramidTest {
+
+    fun pyramid(height: Int) = (1..height).map { " ".repeat(height - it) + "*".repeat(2 * it - 1) }
+
+    @Test
+    fun testPyramid() {
+        assertThat(pyramid(0)).contains();
+        assertThat(pyramid(1)).contains("*");
+        assertThat(pyramid(2)).contains(" *", "***");
+        assertThat(pyramid(3)).contains("  *", " ***", "*****")
+    }
+
+
+}
