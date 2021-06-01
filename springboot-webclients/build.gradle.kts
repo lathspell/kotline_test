c2b549f847cbe29b856843b5f7ab615dba1d3272
Christian Brunotte <christian.brunotte@obi.de>
2021-06-01 23:09:20 +0200

    dependencies

--- springboot-webclients/build.gradle.kts
+++ springboot-webclients/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.4.30"
-	kotlin("plugin.spring") version "1.4.30"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	id("org.springframework.boot") version "2.4.1"
+	kotlin("jvm") version "1.5.10"
+	kotlin("plugin.spring") version "1.5.10"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.springframework.boot") version "2.5.0"
 }
 
 group = "de.lathspell.test"
@@ -12,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	jcenter()
+	mavenCentral()
 }
 
 dependencies {
